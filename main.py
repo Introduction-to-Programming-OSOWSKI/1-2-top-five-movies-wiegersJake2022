4aec383d54c2def55012160e481041b52e4b17a5
wiegerJake2022 <824005#@mystma.org>
2022-01-07 08:49:56 -0600

    7

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveovies(a,b,c,d,e):
-    return  a+" "+b+" "+c+ " " +d+" "+e 
+    return  a," ",b," ",c, " " ,d," ",e 
 topFiveovies("thor", "Iron man", "Hulk", "Spider Man", "Doctor Strange")
