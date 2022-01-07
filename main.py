34012d204779fee327a9f03bd0bb5c39e4919562
wiegerJake2022 <824005#@mystma.org>
2022-01-07 08:47:21 -0600

    6

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveovies(a,b,c,d,e):
     return  a+" "+b+" "+c+ " " +d+" "+e 
-topFiveMovies("thor", "Iron man", "Hulk", "Spider Man", "Doctor Strange")
+topFiveovies("thor", "Iron man", "Hulk", "Spider Man", "Doctor Strange")
